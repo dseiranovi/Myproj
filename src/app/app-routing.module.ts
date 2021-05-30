2a4fcf39c04aa935ac9750caa584445afc5697b1
dseiranovi <davitseiranovi@gmail.com>
2021-05-30 21:56:41 +0400

    some changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent} from './login/login.component'
+import { IndexComponent} from './index/index.component'
+import { RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: '',   redirectTo: '/index', pathMatch: 'full'},
+  {path: 'login', component:LoginComponent},
+  {path: 'registration', component:RegistrationComponent},
+  {path: 'index', component:IndexComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { IndexComponent } from './index/index.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    IndexComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
